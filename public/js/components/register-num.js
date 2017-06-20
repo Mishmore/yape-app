f0da13bb907e8aae7b488c12a2ed0ef69ec6f508
Mishmore <michellemore28@gmail.com>
2017-06-20 13:45:30 -0500

    regex num

--- public/js/components/register-num.js
+++ public/js/components/register-num.js
@@ -4,15 +4,35 @@ const RegisterNum = (update) => {
   const parent = $('<div></div>');
   const title = $('<h4>Para comenzar validaremos tu número</h4>');
   const p = $('Recibirás un SMS con un código de validación');
+  const form = $('<form onsubmit="return false"></form>');
   const inputNum = $('<input type="number">');
   const checkbox = $('<input type="checkbox"> Acepto los <a href="#">Términos y condiciones</a>');
-  const btnContinuar = $('<button>CONTINUAR</button>');
+  const btnContinuar = $('<button type="submit">CONTINUAR</button>');
+  btnContinuar.prop('disabled', true);
+
+  const validateNum = () => {
+    if( (/^\d{9}$/.test(inputNum.val())) ) {
+      btnContinuar.prop('disabled', false);
+      console.log('ok');
+    } else {
+      console.log('wrong');
+    }
+  }
+
+  inputNum.on('keyup', (e) => {
+    validateNum();
+  });
+
+  checkbox.on('click', (e) => {
+    validateNum();
+  });
 
   parent.append(title);
   parent.append(p);
-  parent.append(inputNum);
-  parent.append(checkbox);
-  parent.append(btnContinuar);
+  parent.append(form);
+  form.append(inputNum);
+  form.append(checkbox);
+  form.append(btnContinuar);
 
   return parent;
 }
