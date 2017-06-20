3ea63f20120bd81f618991f851635c0c20163235
Mishmore <michellemore28@gmail.com>
2017-06-20 15:24:32 -0500

    textos

--- public/js/components/create-user.js
+++ public/js/components/create-user.js
@@ -4,6 +4,12 @@ const CreateUser = (update) => {
   const parent = $('<div></div>');
   const title = $('<h4>Crea tu usuario Yape</h4>');
   const p = $('<p>Ingresa un nombre, email y clave de usuario</p>');
+  const form = $('<form onsubmit="return false"></form>');
+  const nombre = $('<input type="text">');
+  const email = $('<input type="email">');
+  const password = $('<input type="password">');
+  const btnCreate = $('<button type="submit" name="button">CREAR CUENTA</button>');
+  const recomendation = $('<p>Cuida esta clave como oro, ya que es tu acceso a Yape.</p>');
 
   parent.append(title);
   parent.append(p);
