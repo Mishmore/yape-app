5794c417d280a0e7b2328b4eaefbfd92f9b1999e
Mishmore <michellemore28@gmail.com>
2017-06-20 12:57:44 -0500

    inputs

--- public/js/components/register-num.js
+++ public/js/components/register-num.js
@@ -1,6 +1,18 @@
 'use strict';
 
 const RegisterNum = (update) => {
-  const btn = $('<button>hola</button>');
-  return btn;
+  const parent = $('<div></div>');
+  const title = $('<h4>Para comenzar validaremos tu número</h4>');
+  const p = $('Recibirás un SMS con un código de validación');
+  const inputNum = $('<input type="number">');
+  const checkbox = $('<input type="checkbox"> Acepto los <a href="#">Términos y condiciones</a>');
+  const btnContinuar = $('<button>CONTINUAR</button>');
+
+  parent.append(title);
+  parent.append(p);
+  parent.append(inputNum);
+  parent.append(checkbox);
+  parent.append(btnContinuar);
+
+  return parent;
 }
